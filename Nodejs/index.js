c7178adb1435ce4b05625b5cbaf899547642181e
Dang Khoa <dangkhoa8102@gmail.com>
2024-04-15 19:40:46 +0700

    last 4/15/2024

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,1 +1,8 @@
-console.log("aaaaa");
+import {sumTwoNum, printConsole} from './util.js'
+import subTwoNum from './util.js'
+
+ function main() {
+    printConsole("tong 2 so 1 & 2:" +  sumTwoNum(1, 2));
+    printConsole("hieu 2 so 1 & 2:" +  subTwoNum(1, 2));
+ }
+ main( );
---
+++ Nodejs/util.js
@@ -0,0 +1,10 @@
+export function sumTwoNum (numA, numB) {
+    return numA + numB;
+}
+export function printConsole(msg){
+    return console.log(msg);
+}
+
+export default function subTwoNum( numA, numB) {
+    return numA - numB;
+}
